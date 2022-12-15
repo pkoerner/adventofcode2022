ac6a16aea138c02dae531a94edee1e310600934d
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2022-12-15 19:16:47 +0100

    day 15
    This was my worst day so far. Maybe not ever, but so many mistakes just piled up.
    First, I did not parse the - of the negative number in the input. And it took a while to catch on.
    Then I retrieved the y coordinate for Part 2 and tested the code on the small grid size of the example input.
    Guess what var I re-defined and never switched back.
    Unlucky.

---
+++ day15.clj
@@ -0,0 +1,58 @@
+(require 'clojure.string)
+(def input (map (fn [line] (map read-string (re-seq #"-?\d+" line))) (clojure.string/split-lines (slurp "input15"))))
+
+;; Part 1 
+(defn abs' [x] (if (neg? x) (- x) x))
+
+(defn manhattan [[x1 y1 x2 y2]]
+  (+ (abs' (- x1 x2)) (abs' (- y1 y2))))
+
+(defn blocks-at [level [sensor-x sensor-y beacon-x beacon-y]]
+  (let [dist (manhattan [sensor-x sensor-y beacon-x beacon-y])
+        y-dist (abs' (- level sensor-y))
+        x-dist (- dist y-dist)]
+    (for [x (range (- x-dist) (inc x-dist))]
+      (+ x sensor-x))))
+
+;; manually found a sensor at 2000000 in the input, removing that
+(count (disj (set (mapcat (partial blocks-at 2000000) input)) 1690952))
+
+(count (disj (set (mapcat (partial blocks-at 2000000) (take 1 input))) 1690952))
+
+
+;; Part 2
+
+;; switch to intervals
+
+(defn blocks-at [level [sensor-x sensor-y beacon-x beacon-y]]
+  (let [dist (manhattan [sensor-x sensor-y beacon-x beacon-y])
+        y-dist (abs' (- level sensor-y))
+        x-dist (- dist y-dist)]
+    (when (pos? x-dist)
+      [(- sensor-x x-dist) (+ sensor-x x-dist 1)])))
+
+(defn merge-intervals [is]
+  (let [sorted (sort-by first (remove nil? is))]
+    (reduce
+      (fn [a [ilow ihi]]
+        (let [[lo hi] (last a)]
+          (cond (<= lo ilow ihi hi)
+                a
+                (<= lo ilow hi ihi)
+                (conj (vec (butlast a)) [lo ihi])
+                :otherwise
+                (conj a [ilow ihi]))))
+      [(first sorted)]
+      (rest sorted))))
+
+(defn covered-by? [[lo hi] [blo bhi]]
+  (<= blo lo hi bhi))
+(defn covered? [intervals i]
+  (some (partial covered-by? i) intervals))
+
+(def gridsize 4000000)
+(def y-coord (first (for [y (range 0 (inc gridsize))
+                          :when (not (covered? (merge-intervals (map (partial blocks-at y) input)) [0 gridsize]))]
+                      y)))
+
+(+ y-coord (* gridsize (get-in (merge-intervals (map (partial blocks-at y-coord) input)) [0 1])))
